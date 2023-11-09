1bc4c5d942523110b152630a81e68c842e8e145b
DipuBarua <you@example.com>
2023-11-09 19:56:52 +0600

    delete and get operation for service provider table

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -24,13 +24,14 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const homegrownCollection = client.db('homegrownDB').collection('products');
         const bookingCollection = client.db('homegrownDB').collection('bookings');
 
         // products -- API operations 
         app.get('/products', async (req, res) => {
+            // console.log(req.query);
             const cursor = homegrownCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -43,6 +44,21 @@ async function run() {
             res.send(result);
         })
 
+        // to manage products/services by indiviually (service owner) 
+        app.get('/products', async (req, res) => {
+            console.log(req.query);
+            console.log(req.query.email);
+
+            let query = {}
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await homegrownCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
+
         app.post('/products', async (req, res) => {
             const newService = req.body;
             console.log(newService);
@@ -50,8 +66,15 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await homegrownCollection.deleteOne(query);
+            res.send(result);
+        })
 
-        
+
+
         // bookings -- API operations 
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
@@ -62,7 +85,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
